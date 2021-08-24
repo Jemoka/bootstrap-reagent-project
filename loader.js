100edef434076ee230bbf909a28f486bb41b5260
Houjun Liu <hliu@shabang.cf>
2021-08-23 23:24:16 -0700

    argggg

--- loader.js
+++ loader.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const { spawn } = require('child_process');
-const child = spawn('/usr/bin/env', ['bash', './bootstrap.sh'], {stdio: 'inherit'});
+const path = require('path');
+
+const child = spawn('/usr/bin/env', ['bash', path.join(__dirname, 'bootstrap.sh')], {stdio: 'inherit'});
 
 
